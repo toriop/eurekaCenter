8b3d32ca6be9637275794c493477331139258533
junhui <junhhe@cisco.com>
2020-11-17 15:50:19 +0800

    ase enter the commit message for your changes. Lines starting

---
+++ src/test/java/com/start/eurekaCenter/EurekaCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.start.eurekaCenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
